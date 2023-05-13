95e0eef6fbb14ce939f0fa25071c9b2cef5a6e81
Michael Gilos <gilosmichael@gmail.com>
2023-05-13 16:48:45 +0800

    working product item

--- src/features/product/ProductList.tsx
+++ src/features/product/ProductList.tsx
@@ -1,39 +1,51 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  View,
   FlatList,
   Image,
+  SafeAreaView,
+  StyleSheet,
   Text,
+  View,
 } from 'react-native';
 import {useGetAllProducts} from './hooks/useGetAllProducts';
 
-const ProductList = () => {
-  const {data, isLoading, refetch, isError, isSuccess} = useGetAllProducts();
+type ItemProps = {
+  id: string;
+  title: string;
+  thumbnail: string;
+  price: number;
+  rating: number;
+  date: string;
+};
+const Item = ({title, thumbnail, price, rating, date}: ItemProps) => (
+  <View style={styles.item}>
+    <Text style={styles.itemTitle}>{title}</Text>
+    <Image style={styles.imageThumbnail} source={{uri: thumbnail}} />
+    <Text style={styles.itemText}>{`$${price}`}</Text>
+    <Text style={styles.itemText}>{new Date(date).toLocaleString()}</Text>
+  </View>
+);
 
-  if (!isLoading) {
-    return (
-      <View style={styles.container}>
-        <Text>loading...</Text>
-      </View>
-    );
+const Separator = () => (
+  <View style={{height: 1, backgroundColor: 'grey', marginHorizontal: 10}} />
+);
+
+const ProductList = () => {
+  const {data, isLoading, refetch} = useGetAllProducts();
+
+  if (isLoading) {
+    return <Text style={{color: 'black'}}>loading...</Text>;
   }
 
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
+        style={{margin: 10}}
+        refreshing={isLoading}
         data={data}
-        renderItem={({item}) => (
-          <View style={{flex: 1, flexDirection: 'column', margin: 1}}>
-            <Image
-              style={styles.imageThumbnail}
-              source={{uri: item.thumbnail}}
-            />
-          </View>
-        )}
-        numColumns={3}
-        keyExtractor={(item, index) => index}
+        renderItem={({item}) => <Item {...item} />}
+        numColumns={2}
+        keyExtractor={item => item.id}
       />
     </SafeAreaView>
   );
@@ -45,11 +57,25 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    backgroundColor: 'white',
+    backgroundColor: 'blue',
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    margin: 2,
+    // marginVertical: 8,
+    // marginHorizontal: 16,
+  },
+  itemTitle: {
+    fontSize: 16,
+  },
+  itemText: {
+    fontSize: 12,
   },
   imageThumbnail: {
     justifyContent: 'center',
     alignItems: 'center',
     height: 100,
+    width: 100,
   },
 });
