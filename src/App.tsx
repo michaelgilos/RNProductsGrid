4f83f3853c38b93f13dc43b6998c30b1e29967ed
Michael Gilos <gilosmichael@gmail.com>
2023-05-13 15:34:52 +0800

    wrap query provider

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,30 @@
  * @format
  */
 
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import React from 'react';
-import { Image, SafeAreaView, StatusBar, StyleSheet, Text } from 'react-native';
+import {Image, SafeAreaView, StatusBar, StyleSheet, Text} from 'react-native';
+import ProductList from './features/product/ProductList';
+
+const queryClient = new QueryClient();
 
 function App(): JSX.Element {
   return (
-    <SafeAreaView>
-      <StatusBar barStyle="dark-content" />
+    <QueryClientProvider client={queryClient}>
+      <SafeAreaView>
+        <StatusBar barStyle="dark-content" />
 
-      <Text>Product Grid</Text>
+        {/* <Text>Product Grid</Text>
       <Text>But first, a word from our sponsors:</Text>
       <Image
         source={{
           uri: `http://localhost:8000/ads/?r=${Math.floor(Math.random()*1000)}`,
         }}
         style={styles.ads}
-      />
-    </SafeAreaView>
+      /> */}
+        <ProductList />
+      </SafeAreaView>
+    </QueryClientProvider>
   );
 }
 
